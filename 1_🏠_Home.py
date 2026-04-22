520893962978d831d3e27125f140e9f2405d0ea7
Renbee Misamen <renbeemisamen54@gmail.com>
2026-04-22 11:05:37 +0800

    Add files via upload

---
+++ 1_🏠_Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("🏠 Home")
+
+st.header("Hi, I'm Renbee Misamen 👋")
+st.write("Aspiring CS Student | Future Developer 💻")
+
+st.image("photo.jpeg", caption="My Banner", width=300)
+
+st.info("Welcome to my portfolio! Explore my work and skills.")
---
+++ 3_💻_Skills.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("💻 Skills")
+
+st.subheader("Programming")
+st.progress(70)
+st.write("Python")
+
+st.progress(60)
+st.write("JavaScript")
+
+st.progress(15)
+st.write("PHP")
+
+st.subheader("Design")
+st.progress(80)
+st.write("Canva / UI Design")
+
+st.subheader("Tools")
+st.write("- GitHub")
+st.write("- VS Code")
+st.write("- Streamlit")
