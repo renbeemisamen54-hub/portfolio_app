9a446fb400eacfe7613f0360d81b167d49309d8a
Renbee Misamen <renbeemisamen54@gmail.com>
2026-04-23 09:28:40 +0800

    Add files via upload

---
+++ portfolio_app/Dashboard.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Portfolio", page_icon="🌐")
+
+st.title("🌟 Welcome to My Portfolio")
+
+st.write("""
+Navigate through my portfolio using the sidebar.
+
+Sections included:
+- Home
+- About Me
+- Skills
+- Projects
+- Contact
+""")
+
+st.success("Simple Portfolio Multipage App using Streamlit")
---
+++ portfolio_app/pages/1_🏠_Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("🏠 Home")
+
+st.header("Hi, I'm Renbee Misamen 👋")
+st.write("Aspiring CS Student | Future Developer 💻")
+
+st.image("C:/Users/Renbee Misamen/OneDrive/Pictures/Camera Roll/IMG_20250529_095602_061609.jpg", caption="My Banner", width=300)
+
+st.info("Welcome to my portfolio! Explore my work and skills.")
