dd26bd6d5ae03228928bf5793272e704c36c7cc9
Renbee Misamen <renbeemisamen54@gmail.com>
2026-04-23 09:42:25 +0800

    Update 1_🏠_Home.py

--- portfolio_app/pages/1_🏠_Home.py
+++ portfolio_app/pages/1_🏠_Home.py
@@ -5,6 +5,6 @@ st.title("🏠 Home")
 st.header("Hi, I'm Renbee Misamen 👋")
 st.write("Aspiring CS Student | Future Developer 💻")
 
-st.image("IMG_20250529_095602_061609.jpg", caption="My Banner", width=300)
+st.image("portfolio_app/IMG_20250529_095602_061609.jpg", caption="My Banner", width=300)
 
 st.info("Welcome to my portfolio! Explore my work and skills.")
